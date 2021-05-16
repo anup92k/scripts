0834a1093a06efc8568adec7318e6730a2a682d7
anup92k <anup.sungum@gmail.com>
2021-05-16 18:34:50 +0200

    Add script to notify about system updates

---
+++ update-notifier/README.md
@@ -0,0 +1,39 @@
+## Gotify notification about updates
+
+The purpose of this script is to get a notification 
+about available updates using 
+[Gotify](https://gotify.net) with cURL.
+
+
+### Installation
+#### Configuration file (optional)
+
+Copy `gotify-notify.conf` to the `/etc/` folder or be lazy :
+```
+wget https://raw.githubusercontent.com/anup92k/scripts/master/update-notifier/gotify-notify.conf -O /etc/gotify-notify.conf
+```
+
+Don't forget to update it with your personnal credentials !
+
+
+#### Script
+```
+wget https://raw.githubusercontent.com/anup92k/scripts/master/power-notify/my-unattended-upgrades-notify.sh -O /usr/local/sbin/my-unattended-upgrades-notify.sh
+chmod u+x /usr/local/sbin/my-unattended-upgrades-notify.sh
+```
+
+If you are not using the configuration file,
+don't forget to update thoses variables :
+
+* `GOTIFY_URL`
+* `GOTIFY_TOKEN`
+
+
+#### Cron
+
+I run this script every day at 8 o'clock on my servers using `cron`.
+
+Here is my definition :
+```
+0 8 * * * /usr/local/sbin/my-unattended-upgrades-notify.sh
+```
---
+++ update-notifier/my-unattended-upgrades-notify.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Send Gotify notifications about
+# ubuntu pending updates
+
+# Gotify parameters (if not using configuration file)
+GOTIFY_URL="https://gotify.blablabla.mu"
+GOTIFY_TOKEN="Az3rTy"
+# Tag to see in log file
+LOGGER_TITLE="my-unattended-upgrades-notify"
+
+# Configuration file
+FILE_CONF="/etc/gotify-notify.conf"
+
+# Handling configuration file (if present)
+if [[ -f "${FILE_CONF}" ]]; then
+  GOTIFY_URL=$(grep "server-url=" $FILE_CONF | cut -d'=' -f2)
+  GOTIFY_TOKEN=$(grep "access-token=" $FILE_CONF | cut -d'=' -f2)
+fi
+
+# Server info
+server_name=$(uname -n)
+updates_available=$(cat /var/lib/update-notifier/updates-available | awk NF | grep -v apt)
+release=$(lsb_release -d | cut -d$'\t' -f2)
+
+
+# Handle no updates
+if [[ -z "$updates_available" ]]; then
+  updates_available="No updates available."
+fi
+
+
+# Processing notification content
+## Title
+title="$server_name ($release)"
+
+## Message
+message="$updates_available"
+
+# Finally cURLing !
+curl_http_result=$(curl "${GOTIFY_URL}/message?token=${GOTIFY_TOKEN}" -F "title=${title}" -F "message=${message}" -F "priority=5" --output /dev/null --silent --write-out %{http_code})
+if [[ $? -ne 0 ]]; then
+  logger -t $LOGGER_TITLE "FATAL ERROR: cURL command failed !"
+  exit 1
+fi
+
+# Check HTTP return code ("200" is OK)
+if [[ $curl_http_result -ne 200 ]]; then
+  logger -t $LOGGER_TITLE "FATAL ERROR: API call failed ! Return code is $curl_http_result instead of 200."
+  exit 2
+fi
+
+logger -t $LOGGER_TITLE "Notification sent"
+
+exit 0
